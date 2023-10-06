a57b24028de6747d9988cbfc54e915e39183e7e0
Uche Ofia <coolucheofia@gmail.com>
2023-10-06 16:42:56 +0100

    chore: Update cleanup script

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.config.settings import (
+from app.settings import (
     DB_USER,
     DB_PASSWORD,
     DB_PORT,
