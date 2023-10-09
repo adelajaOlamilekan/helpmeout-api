ff6988b9faa943ada8e5b900e27ff6b6919c4821
Uche Ofia <coolucheofia@gmail.com>
2023-10-09 02:49:28 +0100

    feat: Enforce unique constraint on username-password combination

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -1,8 +1,16 @@
 from typing import Optional
 
 from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Boolean,
+    UniqueConstraint,
+)
 from sqlalchemy.sql import func
+
 from app.database import Base
 
 
@@ -13,10 +21,15 @@ class User(Base):
     username: str = Column(
         String, index=True, unique=True, nullable=False, default=None
     )
-    hashed_password: str = Column(String, unique=True, nullable=False)
+    hashed_password: str = Column(String, nullable=False)
     created_date: DateTime = Column(DateTime, server_default=func.now())
     updated_date: DateTime = Column(DateTime, onupdate=func.now())
     is_deleted: bool = Column(Boolean, default=False)
+
+    # Add a unique constraint on 'username' and 'hashed_password' combination
+    __table_args__ = (
+        UniqueConstraint("username", "hashed_password", name="unique_user"),
+    )
 
 
 class UserRequest(BaseModel):
