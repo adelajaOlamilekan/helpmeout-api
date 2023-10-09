f65e2020d1e49f473b85c290c89022467c24437f
Uche Ofia <coolucheofia@gmail.com>
2023-10-09 04:46:29 +0100

    docs: Update models with appropriate type hints

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -25,7 +25,7 @@ class User(Base):
     )
     hashed_password: str = Column(String, nullable=False)
     created_date: DateTime = Column(DateTime, server_default=func.now())
-    updated_date: DateTime = Column(DateTime, onupdate=func.now())
+    updated_date: Optional[DateTime] = Column(DateTime, onupdate=func.now())
     is_deleted: bool = Column(Boolean, default=False)
 
     # Add a unique constraint on 'username' and 'hashed_password' combination
--- app/models/video_models.py
+++ app/models/video_models.py
@@ -1,5 +1,6 @@
 """ The video models """""
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
 from sqlalchemy import Column, Enum, String, DateTime, ForeignKey
@@ -13,17 +14,17 @@ class Video(Base):
     __tablename__ = "videos"
 
     id: str = Column(String, primary_key=True, unique=True, nullable=False)
-    username = Column(
+    username: str = Column(
         String,
         ForeignKey("users.username", ondelete="CASCADE"),
         nullable=False,
     )
-    created_date = Column(DateTime, default=datetime.utcnow)
-    original_location = Column(String, nullable=True)
-    compressed_location = Column(String, nullable=True)
-    thumbnail_location = Column(String, nullable=True)
-    transcript_location = Column(String, nullable=True)
-    status = Column(
+    created_date: datetime = Column(DateTime, default=datetime.utcnow)
+    original_location: Optional[str] = Column(String, nullable=True)
+    compressed_location: Optional[str] = Column(String, nullable=True)
+    thumbnail_location: Optional[str] = Column(String, nullable=True)
+    transcript_location: Optional[str] = Column(String, nullable=True)
+    status: str = Column(
         Enum(
             "processing",
             "completed",
