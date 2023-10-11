656f5b99a8b7e961949c2ceb62af7401d16f146a
Adelaja Qowiyyu Olamilekan <adexolamilekan46@gmail.com>
2023-10-11 10:44:02 +0100

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -34,7 +34,6 @@ load_dotenv()
 
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-print(GOOGLE_CLIENT_SECRET, GOOGLE_CLIENT_ID )
 FACEBOOK_CLIENT_ID = os.getenv("FACEBOOK_CLIENT_ID")
 FACEBOOK_CLIENT_SECRET = os.getenv("FACEBOOK_CLIENT_SECRET")
 
@@ -177,9 +176,9 @@ async def logout_user(
 @auth_router.get("/google/login/")
 async def google_login():
     """ Generate Login URL and redirect """
-
-    with google_sso:
-        return await google_sso.get_login_redirect()
+    return {GOOGLE_CLIENT_SECRET: GOOGLE_CLIENT_ID }
+    # with google_sso:
+    #     return await google_sso.get_login_redirect()
 
 @auth_router.get("/google/callback/")
 async def google_callback(request: Request, db: Session = Depends(get_db)) -> UserResponse:
